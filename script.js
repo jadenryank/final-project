38b32e1f84e3a98d25b82778f35ec780fc8a130c
jadenryank <jadenryanknguyen@gmail.com>
2025-04-05 21:49:56 +0000

    main

--- script.js
+++
@@ -1,8 +0,0 @@
-const colors = ["	#ffebcd", "	#e9967a", "8fbc8f", "	#ff1493", "#f0e68c", "#ba55d3"];
-let currentIndex = 0;
-
-function changeColor() {
-  // Set the background to the next color in the array
-  document.body.style.backgroundColor = colors[currentIndex];
-  currentIndex = (currentIndex + 1) % colors.length;
-}
